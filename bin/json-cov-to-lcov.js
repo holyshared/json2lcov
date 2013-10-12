b8a546e2f8b6a76a58ee728f6c5c219c760f552c
holyshared <holy.shared.design@gmail.com>
2013-10-12 15:57:00 +0900

    add cli mode

---
+++ bin/json-cov-to-lcov.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+var inputReport = '',
+    jsoncovToLcov = require('../lib/json-cov-to-lcov');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function(chunk) {
+  inputReport += chunk;
+});
+
+process.stdin.on('end', function() {
+  var lcovResult = '',
+      coverageResult = JSON.parse(inputReport);
+
+  lcovResult = jsoncovToLcov(coverageResult);
+
+  process.stdout.write(lcovResult);
+});
